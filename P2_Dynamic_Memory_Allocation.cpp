aeec9660e9d78db4c3c12fedd6b0738bbe8c3ac1
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-16 08:52:08 +0530

    Add files via upload

---
+++ P2_Dynamic_Memory_Allocation.cpp
@@ -0,0 +1,20 @@
+// C++ program to allocate dynamic memory 
+
+#include<iostream>
+using namespace std;
+
+void function2(int i){
+    int n4;
+}
+
+void function1(){
+    int n3;
+    function2(n3);
+}
+
+int main(){
+    int n1, n2;
+    function1();
+    return 0;
+}
+
