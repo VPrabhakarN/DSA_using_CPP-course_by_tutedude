b7ab980987f6cdf2b6a8c3d8472c4d701854bf35
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-16 08:53:05 +0530

    Add files via upload

---
+++ P6_P4_Time_&_Space_Complexity_ex3.cpp
@@ -0,0 +1,22 @@
+// C++ program to define the time complexity and space complexity
+
+#include<iostream>
+using namespace std;
+
+void add(int n){
+    int i, j;
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            a[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+
+int main(){
+
+    return 0;
+}
+
+// Total time taken = n*n 
+// Time complexity = O(n^2)
+// Space complexity = O(n^2)
