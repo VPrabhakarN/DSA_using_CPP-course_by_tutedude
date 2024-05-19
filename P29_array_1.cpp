d848ec0832ec45de6205ae1dff215e5c324d20aa
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-19 10:02:29 +0530

    Add files via upload

---
+++ P29_array_1.cpp
@@ -0,0 +1,14 @@
+// C++ program to demonstrate the array 
+#include<iostream>
+using namespace std;
+
+int main(){
+    // defining an array 
+    int a[5];
+
+    for(int i=0; i<5; i++){
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
