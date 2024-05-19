81e825a3b6ae6cd2aec76059019786f888712ea3
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-19 10:02:04 +0530

    Add files via upload

---
+++ P30_array_2.cpp
@@ -0,0 +1,16 @@
+// C++ program to demonstrate the array 
+#include<iostream>
+using namespace std;
+
+int main(){
+    // defining an array 
+    int a[5] = {12, 13, 14, 15, 16};
+
+    // accessing one element at one time 
+    cout << a[0] << endl;
+    cout << a[1] << endl;
+    cout << a[2] << endl;
+    cout << a[3] << endl;
+    cout << a[4] << endl;
+    return 0;
+}
