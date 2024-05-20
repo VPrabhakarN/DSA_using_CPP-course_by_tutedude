b326b8be0420a45357925a9dd7a1aac2871b887f
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-20 18:12:49 +0530

    Add files via upload

---
+++ P33_static_array.cpp
@@ -0,0 +1,13 @@
+// C++ program to demonstrate the static array
+#include<iostream>
+using namespace std;
+
+int main(){
+    // defining an array 
+    int a[5] = {1, 2, 3, 4, 5};
+
+    // traversing an array 
+    for(int i=0; i<5; i++){
+        cout << a[i] << " ";
+    }
+}
