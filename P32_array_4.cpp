0ed95246e916cd24e36fe73cf96568d3d3185db7
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-20 18:12:31 +0530

    Add files via upload

---
+++ P32_array_4.cpp
@@ -0,0 +1,13 @@
+// C++ program to demonstrate the array  
+#include<iostream>
+using namespace std;
+
+int main(){
+    // defining an array 
+    int a[5] = {1, 2, 3, 1, 0};
+
+    // traversing an array 
+    for(int i=0; i<5; i++){
+        cout << a[i] << endl;
+    }
+}
