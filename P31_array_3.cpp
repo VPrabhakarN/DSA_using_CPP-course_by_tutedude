ae3e8722c937513ed8eb80785ecc61c641caa632
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-20 18:12:13 +0530

    Add files via upload

---
+++ P31_array_3.cpp
@@ -0,0 +1,13 @@
+// C++ program to demonstrate an array 
+#include<iostream>
+using namespace std;
+
+int main(){
+    // defining an array 
+    int a[] = {12,11,14,14,5,13,44};
+
+    // traversing an array 
+    for(int i=0; i<7; i++){
+        cout << a[i] << endl;
+    }
+}
