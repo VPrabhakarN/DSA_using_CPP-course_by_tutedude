8fdaa972d0fd53a28c7ebd22978b95b11148cd5b
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-20 18:13:33 +0530

    Add files via upload

---
+++ P36_2D_array_method_1.cpp
@@ -0,0 +1,13 @@
+// C++ program to demonstrate the 2D array 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[2][3]{
+        {1,2,3},
+        {4,5,6}
+    };
+
+    cout << "This is an example of 2D array....";
+    return 0;
+}
