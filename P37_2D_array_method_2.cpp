3c09bbc2e8094cc0412b16aed1547965e10a68c2
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-20 18:14:23 +0530

    Add files via upload

---
+++ P37_2D_array_method_2.cpp
@@ -0,0 +1,13 @@
+// C++ program to demonstrate the the 2D array 
+#include<iostream>
+using namespace std;
+
+int main(){
+    // defining 2D array using pointer 
+    int *A[2];
+    a[0] = new int[3];
+    a[1] = new int[3];
+    
+    cout << "This is the example of 2D array using method 2....";
+    return 0;
+}
