b695296840c987cb098fd5ec24734a129c8bbabc
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-05-23 20:31:44 +0530

    Add files via upload

---
+++ P42_self_try_example_1.cpp
@@ -0,0 +1,16 @@
+// C++ program for an example of array
+#include<iostream>
+using namespace std;
+
+int main(){
+    // defining an array 
+    int a[];
+    a[4] = {1,2,3,4};
+
+    // accessing an array element
+    cout << a[0] << endl;
+
+    return 0;
+}
+
+// conclusion : -> So, In the case of array we cannot initialize size in array after it's definition.
